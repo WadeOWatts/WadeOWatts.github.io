c8b0093d5729478746b76a18c1cc09ac0724cf91
WadeOWatts <chunwei.chang2@gmail.com>
2024-01-16 19:47:04 +0800

    first copy from chatgpt

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript for Wade's Blog
+function showSearch() {
+    document.getElementById('searchBox').style.display = 'block';
+}
+
+function hideSearch() {
+    document.getElementById('searchBox').style.display = 'none';
+}
