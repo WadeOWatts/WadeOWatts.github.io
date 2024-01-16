a1995552791f4897f9c09f7a7c2a27dd6ba20f1f
WadeOWatts <chunwei.chang2@gmail.com>
2024-01-16 21:17:37 +0800

    add search bar

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-// JavaScript for Wade's Blog
-function showSearch() {
-    document.getElementById('searchBox').style.display = 'block';
-}
+document.addEventListener('DOMContentLoaded', function() {
+    var searchBar = document.getElementById('search-bar');
+    var searchIcon = document.getElementById('search-icon');
 
-function hideSearch() {
-    document.getElementById('searchBox').style.display = 'none';
-}
+    searchIcon.onclick = function() {
+        if (searchBar.style.display === 'none' || searchBar.style.display === '') {
+            searchBar.style.display = 'block';
+            searchIcon.src = 'https://e7.pngegg.com/pngimages/666/635/png-clipart-computer-icons-cancel-angle-logo-thumbnail.png'; // URL of the 'X' icon
+        } else {
+            searchBar.style.display = 'none';
+            searchIcon.src = 'https://www.clarin.eu/sites/default/files/styles/medium/public/icon-services-fcs.png?itok=seug9Hqn'; // URL of the search icon
+        }
+    };
+});
+
