5f38d8000b5e2a8f5e11a7f276b4f649e95bd91b
WadeOWatts <chunwei.chang2@gmail.com>
2024-01-18 09:43:10 +0800

    take out sidebar

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
     var searchBar = document.getElementById('search-bar');
     var searchIcon = document.getElementById('search-icon');
 
     searchIcon.onclick = function() {
         if (searchBar.style.display === 'none' || searchBar.style.display === '') {
             searchBar.style.display = 'block';
-            searchIcon.src = 'https://e7.pngegg.com/pngimages/666/635/png-clipart-computer-icons-cancel-angle-logo-thumbnail.png'; // URL of the 'X' icon
+            searchIcon.src = 'https://flaticons.net/icon.php?slug_category=mobile-application&slug_icon=close'; // URL of the 'X' icon
         } else {
             searchBar.style.display = 'none';
             searchIcon.src = 'https://static-00.iconduck.com/assets.00/search-icon-2044x2048-psdrpqwp.png'; // URL of the search icon
         }
     };
-});
+});*/
+
+function toggleSearch() {
+    var searchBar = document.getElementById("search-bar");
+    var searchIcon = document.getElementById("search-icon");
+    var closeIcon = document.getElementById("close-icon");
+
+    if (searchBar.style.display === "none") {
+        searchBar.style.display = "block";
+        searchIcon.style.display = "none";
+        closeIcon.style.display = "block";
+    } else {
+        searchBar.style.display = "none";
+        searchIcon.style.display = "block";
+        closeIcon.style.display = "none";
+    }
+}
 
 // This script toggles the dropdown menus on click
 document.addEventListener('DOMContentLoaded', function() {
